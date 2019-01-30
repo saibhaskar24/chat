05d8691030d25b4f71d4d922e54694adafb98c8f
sai bhaskar <35323355+saibhaskar24@users.noreply.github.com>
2019-01-30 21:42:37 +0530

    all users

--- app/src/main/java/com/example/bhaskar/chat/AllUsers.java
+++ app/src/main/java/com/example/bhaskar/chat/AllUsers.java
@@ -2,16 +2,19 @@ package com.example.bhaskar.chat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
 public class AllUsers extends AppCompatActivity {
 
     Toolbar toolbar;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_users);
+        recyclerView = (RecyclerView) findViewById(R.id.allusers_list);
         toolbar = (Toolbar)findViewById(R.id.toolbar);
         toolbar.setTitle("All Users");
         setSupportActionBar(toolbar);
